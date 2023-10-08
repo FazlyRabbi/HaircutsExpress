33b991fbb7ff0941e605bb2ad6ed01d419017e2e
FazlyRabbi <rabbisam74@gmail.com>
2023-10-08 16:14:26 +0600

    the erros

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,11 @@
 "use client";
-
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Service from "./components/Service";
-import Work from "./components/Work";
-import Worktime from "./components/Worktime";
-
 // imports components
+import Home from "./components/Home/Home";
 
-export default function Home() {
+export default function Index() {
   return (
     <>
-      <Header/>
-      <Hero/>
-      <Service/>
-      <Worktime/>
-      <Work/>
-      <Footer/>
+      <Home />
     </>
   );
 }
